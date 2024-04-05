537cae46b6cd6fbeb0e0d7e2dc7e5e79eb7401c0
ashhar <ashharulhaque@gmail.com>
2024-04-06 00:12:08 +0530

    change

--- src/app/(routescomponent)/layout.jsx
+++ src/app/(routescomponent)/layout.jsx
@@ -1,7 +1,17 @@
-function LayoutRoutesComponent({children}) {
+function LayoutRoutesComponent({ children }) {
   return (
-    <div>{children}</div>
-  )
+    <>
+    <div className="bg-green-400 flex flex-col min-h-[100vh]">
+    <header className="bg-red-400 ">header</header>
+      <aside className="bg-yellow-300">Sidebar</aside>
+      <div className="bg-black">{children}</div>
+      <footer className="bg-fuchsia-600 min-h[40dvh]">
+        <h1>footer</h1>
+      </footer>
+    </div>
+     
+    </>
+  );
 }
 
-export default LayoutRoutesComponent
+export default LayoutRoutesComponent;
