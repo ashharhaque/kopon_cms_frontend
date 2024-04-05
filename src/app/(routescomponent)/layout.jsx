e539b59b060b7e4bbd8f000a6432975df2da136b
ashhar <ashharulhaque@gmail.com>
2024-04-06 00:21:22 +0530

    height done

--- src/app/(routescomponent)/layout.jsx
+++ src/app/(routescomponent)/layout.jsx
@@ -1,15 +1,17 @@
 function LayoutRoutesComponent({ children }) {
   return (
     <>
-    <div className="bg-green-400 flex flex-col min-h-[100vh]">
-    <header className="bg-red-400 ">header</header>
-      <aside className="bg-yellow-300">Sidebar</aside>
-      <div className="bg-black">{children}</div>
-      <footer className="bg-fuchsia-600 min-h[40dvh]">
-        <h1>footer</h1>
-      </footer>
-    </div>
-     
+      <div className="bg-green-400 flex flex-col justify-between min-h-[100dvh]">
+        <header className="bg-red-400 h-[10dvh]">header</header>
+        <div className="bg-red-500 flex min-h-[80dvh]">
+          <aside className="bg-yellow-300 ">Sidebar</aside>
+          <div className="bg-black text-white">{children}</div>
+        </div>
+
+        <footer className="bg-fuchsia-600 min-h-[10dvh]">
+          <h1>footer</h1>
+        </footer>
+      </div>
     </>
   );
 }
