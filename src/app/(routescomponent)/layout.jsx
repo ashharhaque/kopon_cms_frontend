054d3fbece9c9e309da899edf019a0fd02cc62b1
ashhar <ashharulhaque@gmail.com>
2024-04-06 01:07:44 +0530

    change

--- src/app/(routescomponent)/layout.jsx
+++ src/app/(routescomponent)/layout.jsx
@@ -1,13 +1,20 @@
+"use client"
+import { useState } from "react";
 import { ExpandSideBarIcon } from "@/icons/icons";
 function LayoutRoutesComponent({ children }) {
+  const [sideBarCollapse,setSideBarCollapse]=useState(false);
+  const sideBarCollapseFunc=()=>{
+    console.log("inside sideBarToggleFunc--->")
+    setSideBarCollapse(!sideBarCollapse);
+  }
   return (
     <>
       <div className="bg-green-400 flex flex-col justify-between min-h-[100dvh]">
         <header className="bg-red-400 h-[10dvh]">header</header>
         <div className="bg-red-500 flex min-h-[80dvh]">
           <aside className="bg-yellow-300 flex  w-[15dvw] relative">
-            <h1 className="bg-green-400 w-[100%]">Sidebar</h1>
-            <ExpandSideBarIcon className={"bg-pink-300 w-[4dvw] h-[4dvh] absolute top-1/2 right-0"} />
+            <h1 className={`bg-green-400 w-[100%] transition-all ease-in-out  duration-1000 ${sideBarCollapse?"w-[30%]":"w-[100%]"}`}>Sidebar</h1>
+            <span onClick={sideBarCollapseFunc}><ExpandSideBarIcon className={"bg-pink-300 w-[4dvw] h-[4dvh] absolute top-1/2 right-0"} /></span>
           </aside>
           <div className="bg-blue-600 text-white w-[85dvw]">{children}</div>
         </div>
