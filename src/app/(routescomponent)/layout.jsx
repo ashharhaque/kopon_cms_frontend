62a3182a45d97a77f9b189eae318028c66dabf8f
ashhar <ashharulhaque@gmail.com>
2024-04-06 00:24:45 +0530

    side bar done

--- src/app/(routescomponent)/layout.jsx
+++ src/app/(routescomponent)/layout.jsx
@@ -4,8 +4,8 @@ function LayoutRoutesComponent({ children }) {
       <div className="bg-green-400 flex flex-col justify-between min-h-[100dvh]">
         <header className="bg-red-400 h-[10dvh]">header</header>
         <div className="bg-red-500 flex min-h-[80dvh]">
-          <aside className="bg-yellow-300 ">Sidebar</aside>
-          <div className="bg-black text-white">{children}</div>
+          <aside className="bg-yellow-300 w-[15dvw]">Sidebar</aside>
+          <div className="bg-black text-white w-[85dvw]">{children}</div>
         </div>
 
         <footer className="bg-fuchsia-600 min-h-[10dvh]">
