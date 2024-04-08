d67f9df0b05d1554a3c2f5c08849185a22babd3d
ashhar <ashharulhaque@gmail.com>
2024-04-08 15:14:56 +0530

    change

---
+++ src/app/(routescomponent)/restaurants/Summary.js
@@ -0,0 +1,5 @@
+function SummaryOfRestaurant({className,children}) {
+  return <div className={className}>Summary of Restaurant</div>;
+}
+
+export default SummaryOfRestaurant;
--- src/app/(routescomponent)/restaurants/page.js
+++ src/app/(routescomponent)/restaurants/page.js
@@ -1,5 +1,14 @@
+import SummaryOfRestaurant from "@/app/(routescomponent)/restaurants/Summary";
+
 function Restaurants() {
-  return <div className="bg-pink-400 min-h-[70dvh]">Restaurant</div>;
+  return (
+    <>
+      <div className="bg-yellow-200 flex flex-1">
+        <div className="bg-pink-400 min-h-[70dvh] w-[80%]">Restaurant</div>
+        <SummaryOfRestaurant className="bg-green-300 w-[20%]"/>
+      </div>
+    </>
+  );
 }
 
 export default Restaurants;
