01d948c23bf112118647eee08d07e6febde822a9
ashhar <ashharulhaque@gmail.com>
2024-04-10 12:33:09 +0530

    restaurant project

--- src/app/(routescomponent)/restaurants/restaurant/layout.js
+++ src/app/(routescomponent)/restaurants/restaurant/layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from "next/image";
+import ToggleButton from "@/ui/ToggleButton"
 function SingleRestaurantLayout({children}) {
   return (
     <>
-    <div className='bg-pink-400 flex  w-[100%] min-h-[15vh] '>
-        <div className='bg-yellow-300 flex justify-center w-[10dvw] h-[15vh] '>
+    <div className='bg-pink-400 flex items-center w-[100%] min-h-[15vh] '>
+        <div className='bg-yellow-300 flex justify-center items-center w-[10dvw] h-[15vh] '>
             <Image src="/images/restaurantImageBus.jpeg"
             width={90}
             height={90}
@@ -12,8 +13,12 @@ function SingleRestaurantLayout({children}) {
             className='object-cover '
             />
         </div>
-        <p className='bg-red-600 flex items-center'>The Family Masala</p>
+        <p className='flex items-center mx-2 px-2'>The Family Masala</p>
+        <div className='flex items-center h-[10dvh] '>
+    <span className=' mx-3'>Is Restaurant Active</span><ToggleButton className="px-2"/>
     </div>
+    </div>
+ 
     <div>{children}</div>
     </>
     
