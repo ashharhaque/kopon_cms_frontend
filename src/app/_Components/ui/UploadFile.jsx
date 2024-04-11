d68460541e26374193a58845a1abfd39f5e15228
ashhar <ashharulhaque@gmail.com>
2024-04-11 19:44:23 +0530

    upload file done

---
+++ src/app/_Components/ui/UploadFile.jsx
@@ -0,0 +1,39 @@
+function UploadFile() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      // You can perform file upload logic here, such as using FormData to upload the file to a server
+      console.log("Uploading file:", selectedFile.name);
+    }
+  };
+  return (
+    <form className="max-w-lg mx-auto">
+      <label
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        htmlFor="user_avatar"
+      >
+        Upload file
+      </label>
+      <input
+        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+        aria-describedby="user_avatar_help"
+        id="user_avatar"
+        type="file"
+        onChange={handleFileChange}
+      />
+      <div
+        className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+        id="user_avatar_help"
+      >
+        A profile picture is useful to confirm you are logged into your account
+      </div>
+    </form>
+  );
+}
+
+export default UploadFile;
