68c1efbb2383012f0285000adfc0ea4a2481b6d9
ashhar <ashharulhaque@gmail.com>
2024-04-11 20:55:42 +0530

    handle select

--- src/app/_Components/ui/DropDowns.jsx
+++ src/app/_Components/ui/DropDowns.jsx
@@ -2,10 +2,20 @@ import { useState } from "react";
 
 function Dropdown({ className, dropDownTitle,options }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption,setSelectedOption]=useState(dropDownTitle);
+  
   const OPTIONS_LIST = (option, index) => {
+    const handleOptionSelect = (target_element) => {
+        if (target_element) {
+          const value = target_element.textContent;
+          setSelectedOption(value)
+        }
+      };
     return (
       <>
-        <li>{option}</li>
+        <li key={index} onClick={(e)=>{
+            handleOptionSelect(e.target)
+        }}>{option}</li>
       </>
     );
   };
@@ -17,7 +27,7 @@ function Dropdown({ className, dropDownTitle,options }) {
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
-        {dropDownTitle}{" "}
+        {selectedOption}{" "}
         <svg
           className={`w-2.5 h-2.5 ms-3 transform transition-transform ${
             isOpen ? "rotate-180" : ""
