d906521149d84fa46a57ba992e546f919839e8a5
ashhar <ashharulhaque@gmail.com>
2024-04-11 22:08:36 +0530

    active status done

--- src/app/(routescomponent)/restaurants/restaurant/EditRestaurant.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/EditRestaurant.jsx
@@ -4,6 +4,8 @@ import Modal from "@/ui/Modal";
 import UploadImage from "@/app/_Components/ui/UploadImage";
 import Dropdown from "@/app/_Components/ui/DropDowns";
 import Timings from "@/ui/Timings";
+import ToggleButton from "@/ui/ToggleButton";
+
 function EditRestaurant() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -14,7 +16,7 @@ function EditRestaurant() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
- 
+
   return (
     <>
       <div>
@@ -31,13 +33,42 @@ function EditRestaurant() {
         >
           <div>
             <UploadImage />
-            <div className="flex justify-around ">
-            <Dropdown dropDownTitle={"Select Day"} options={["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"]}/>
-            <Timings className="bg-red-400 flex justify-center items-center" timing_title={"Opening Time"}/>
-            <Timings className="bg-red-400 flex justify-center items-center" timing_title={"Closing Time"}/>
+            <div className="bg-yellow-300 flex flex-col justify-center items-center m-5">
+              <p className="text-black text-center">
+                Restaurant Name {" :"}
+                <input
+                  className="bg-green-400 flex justify-center text-center"
+                  type="text"
+                  value={"The Family Masala"}
+                />
+              </p>
             </div>
-            
-
+            <div className="flex justify-around ">
+              <Dropdown
+                dropDownTitle={"Select Day"}
+                options={[
+                  "SUNDAY",
+                  "MONDAY",
+                  "TUESDAY",
+                  "WEDNESDAY",
+                  "THURSDAY",
+                  "FRIDAY",
+                  "SATURDAY",
+                ]}
+              />
+              <div className="flex items-center h-[10dvh] ">
+                <span className=" mx-1 text-black"> Active</span>
+                <ToggleButton className="px-2" />
+              </div>
+              <Timings
+                className="bg-red-400 flex justify-center items-center"
+                timing_title={"Opening Time"}
+              />
+              <Timings
+                className="bg-red-400 flex justify-center items-center"
+                timing_title={"Closing Time"}
+              />
+            </div>
           </div>
         </Modal>
       </div>
