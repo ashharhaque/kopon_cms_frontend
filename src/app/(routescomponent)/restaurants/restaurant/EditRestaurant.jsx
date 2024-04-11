cf197691d0faaddd42d8f484b0bc6d07e1f65a3c
ashhar <ashharulhaque@gmail.com>
2024-04-11 18:26:54 +0530

    change

--- src/app/(routescomponent)/restaurants/restaurant/EditRestaurant.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/EditRestaurant.jsx
@@ -19,7 +19,16 @@ function EditRestaurant() {
         </p>
       </div>
       <div className=" bg-red-300 ">
-        <Modal isOpen={isOpen} openModal={openModal} closeModal={closeModal} title="Terms of Service" />
+        <Modal isOpen={isOpen} openModal={openModal} closeModal={closeModal} title="Edit Restaurant" >
+        <div className="p-4 md:p-5 space-y-4">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
+              </div>
+        </Modal>
       </div>
     </>
   );
--- src/app/_Components/ui/Modal.js
+++ src/app/_Components/ui/Modal.js
@@ -1,7 +1,7 @@
 "use client"
-import { useState } from 'react';
+import { Children, useState } from 'react';
 
-function Modal({isOpen,closeModal,openModal,title}) {
+function Modal({isOpen,closeModal,openModal,title,children}) {
   
 
   return (
@@ -43,21 +43,14 @@ function Modal({isOpen,closeModal,openModal,title}) {
                   <span className="sr-only">Close modal</span>
                 </button>
               </div>
-              <div className="p-4 md:p-5 space-y-4">
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                  With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                  The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                </p>
-              </div>
+             {children}
               <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
                 <button
                   data-modal-hide="default-modal"
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  I accept
+                  SAVE
                 </button>
                 <button
                   data-modal-hide="default-modal"
