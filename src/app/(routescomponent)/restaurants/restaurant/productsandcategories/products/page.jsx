a4b69b1e9913815cb2531f7b8c31fc4b6785f70b
ashhar <ashharulhaque@gmail.com>
2024-04-13 21:47:44 +0530

    done

--- src/app/(routescomponent)/restaurants/restaurant/productsandcategories/products/page.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/productsandcategories/products/page.jsx
@@ -1,16 +1,41 @@
+"use client";
+import { useState } from "react";
 import ProductList from "@/restaurants/restaurant/productsandcategories/products/productList";
 import SearchBar from "@/ui/SearchBar";
-import LinkButton from "@/app/_Components/ui/LinkButton";
+import Modal from "@/app/_Components/ui/Modal";
 function Products({ className, children }) {
+  const [addProduct, setAddProduct] = useState(false);
+
   return (
     <div className={`${className}`}>
       <div className="flex justify-center items-center">
-        <span className={"inline-block w-[10dvw] min-h-[5dvh] py-2  rounded-full bg-green-400 text-center"}>Add Product</span>
+        <span
+          onClick={() => {
+            setAddProduct(!addProduct);
+          }}
+          className={
+            "inline-block w-[10dvw] min-h-[5dvh] py-2  rounded-full bg-green-400 text-center"
+          }
+        >
+          Add Product
+        </span>
         <SearchBar className={"m-4 bg-yellow-300  w-[40dvw] inline-block"} />
       </div>
-      
-
       <ProductList />
+      {addProduct && (
+        <>
+          <Modal
+            isOpen={addProduct}
+            closeModal={() => {
+              setAddProduct(!addProduct);
+            }}
+            
+            title={"ADD PRODUCT"}
+          >
+            <div className={"text-black"}>ADd product</div>
+          </Modal>
+        </>
+      )}
     </div>
   );
 }
