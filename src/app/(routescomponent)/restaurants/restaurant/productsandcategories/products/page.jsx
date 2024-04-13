9ebef995221fc9cc7d49c22d8d098621328a61e2
ashhar <ashharulhaque@gmail.com>
2024-04-13 21:40:22 +0530

    change

--- src/app/(routescomponent)/restaurants/restaurant/productsandcategories/products/page.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/productsandcategories/products/page.jsx
@@ -1,13 +1,18 @@
-import ProductList from "@/restaurants/restaurant/productsandcategories/products/productList"
-import SearchBar from "@/ui/SearchBar"
-function Products({className,children}) {
+import ProductList from "@/restaurants/restaurant/productsandcategories/products/productList";
+import SearchBar from "@/ui/SearchBar";
+import LinkButton from "@/app/_Components/ui/LinkButton";
+function Products({ className, children }) {
   return (
     <div className={`${className}`}>
-        Products
-        <SearchBar className={"m-4"}/>
-        <ProductList/>
+      <div className="flex justify-center items-center">
+        <span className={"inline-block w-[10dvw] min-h-[5dvh] py-2  rounded-full bg-green-400 text-center"}>Add Product</span>
+        <SearchBar className={"m-4 bg-yellow-300  w-[40dvw] inline-block"} />
+      </div>
+      
+
+      <ProductList />
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
--- src/app/_Components/ui/SearchBar.jsx
+++ src/app/_Components/ui/SearchBar.jsx
@@ -2,7 +2,8 @@
 
 function SearchBar({className}) {
     return (
-        <form className={`${className} max-w-md mx-auto`}>
+        <div className={`${className}`}>
+            <form className={` max-w-md mx-auto`}>
             <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -14,6 +15,8 @@ function SearchBar({className}) {
                 <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
         </form>
+        </div>
+        
     );
 }
 
