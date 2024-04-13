46b327ce558f393c1a66b58d5d54d79cdce391b4
ashhar <ashharulhaque@gmail.com>
2024-04-13 15:56:12 +0530

    categories started

---
+++ src/app/(routescomponent)/restaurants/restaurant/productsandcategories/categories/index.jsx
@@ -0,0 +1,8 @@
+import SearchWithOption from "@/app/_Components/ui/SearchWithOptions";
+function Categories({className,children}) {
+  return <div className={`${className}`}>Categories
+  <SearchWithOption/>
+  </div>;
+}
+
+export default Categories;
