a37f7802697530ce7e243fe5782343f60764b8c4
ashhar <ashharulhaque@gmail.com>
2024-04-16 12:17:26 +0530

    orders

--- src/app/(routescomponent)/restaurants/restaurant/orders/page.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/orders/page.jsx
@@ -2,12 +2,12 @@
 
 function Orders() {
   return (
-    <div>
-      <ul>
-        <li>ALl Orders</li>
-        <li>Dine In Orders</li>
-        <li>Take Away Orders</li>
-        <li>Self Delievery Orders</li>
+    <div className="bg-green-400">
+      <ul className="bg-red-400 flex justify-center w-full min-h-[13dvh]">
+        <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">ALL Orders</li>
+        <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">Dine In Orders</li>
+        <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">Take Away Orders</li>
+        <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">Self Delievery Orders</li>
       </ul>
     </div>
   )
