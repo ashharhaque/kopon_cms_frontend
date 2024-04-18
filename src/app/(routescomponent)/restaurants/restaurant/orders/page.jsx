a004863f65ae6dd9af8568a14c3be96215d4a772
ashhar <ashharulhaque@gmail.com>
2024-04-18 12:02:03 +0530

    done

--- src/app/(routescomponent)/restaurants/restaurant/orders/page.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/orders/page.jsx
@@ -1,5 +1,5 @@
 
-
+import Tabs from "@/ui/Tabs"
 function Orders() {
   return (
     <div className="bg-green-400 flex flex-col ">
@@ -10,6 +10,9 @@ function Orders() {
         <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">Take Away Orders</li>
         <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">Self Delievery Orders</li>
       </ul>
+      <div>
+        <Tabs className={"flex justify-center"}/>
+      </div>
     </div>
   )
 }
