d4e774a1e6d341b609052c73efc414eb7e17ac38
ashhar <ashharulhaque@gmail.com>
2024-04-18 16:58:09 +0530

    orders

--- src/app/(routescomponent)/restaurants/restaurant/orders/OrdersList.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/orders/OrdersList.jsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Image from "next/image";
-import Link from 'next/link'
-import {ForwardIcon} from "@/icons/icons"
+import Link from "next/link";
+import { ForwardIcon } from "@/icons/icons";
 import LinkButton from "@/app/_Components/ui/LinkButton";
 import Modal from "@/app/_Components/ui/Modal";
 import EditCategories from "@/restaurants/restaurant/productsandcategories/categories/EditCategories";
 
 const OrdersList = ({ className }) => {
-  const [editCategories,setEditCategories]=useState(false);
+  const [editCategories, setEditCategories] = useState(false);
   return (
     <>
       <div className={`${className}relative overflow-x-auto shadow-md `}>
@@ -16,19 +16,19 @@ const OrdersList = ({ className }) => {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Logo
+                Order Id
               </th>
               <th scope="col" className="px-6 py-3">
-                Category Name
+                Ordered At
               </th>
               <th scope="col" className="px-6 py-3">
-                Total Products
+                Order Type
               </th>
               <th scope="col" className="px-6 py-3">
-                Active Products
+                Order Status
               </th>
               <th scope="col" className="px-6 py-3">
-                InActive Products
+                Otp
               </th>
               <th scope="col" className="px-6 py-3">
                 Action
@@ -38,26 +38,18 @@ const OrdersList = ({ className }) => {
           <tbody>
             <tr className=" bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                <div className="bg-blue-400 w-[20dvh] h-[20dvh]">
-                  <Image
-                    src="/images/restaurantImageBus.jpeg"
-                    width={100}
-                    height={100}
-                    className="object-cover w-[100%] h-[100%]"
-                  />
-                </div>
+               <span>111</span>
               </td>
               <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Burgers 
+                <span>11:00 a.m</span>
               </td>
               <td className="px-6 py-4 font-medium text-gray-900 text-wrap whitespace-nowrap dark:text-white">
-               100
+                <span className="border-2 border-blue-400 p-3">Dine-In</span>
               </td>
 
-              <td className="px-6 py-4">89</td>
-              <td className="px-6 py-4">11</td>
+              <td className="px-6 py-4 font-medium text-gray-900 text-wrap whitespace-nowrap dark:text-white"><span className=" border-green-300  border-2 p-3">In-Process</span></td>
+              <td className="px-6 py-4 font-medium text-gray-900 text-wrap whitespace-nowrap dark:text-white">1234</td>
               <td className="px-6 py-4">
-               
                 <Link
                   href="/restaurants/restaurant/productsandcategories/products"
                   className="font-medium m-2 text-blue-600 dark:text-blue-500 hover:underline text-center"
@@ -66,18 +58,28 @@ const OrdersList = ({ className }) => {
                   <LinkButton className="">See Products</LinkButton>
                   {/* Edit */}
                 </Link>
-                <LinkButton className={""} ><span onClick={()=>setEditCategories(!editCategories)}>Edit Categories</span></LinkButton>
+                <LinkButton className={""}>
+                  <span onClick={() => setEditCategories(!editCategories)}>
+                    Edit Categories
+                  </span>
+                </LinkButton>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
       <div>
-      {editCategories && <Modal title={"Edit Categories"} isOpen={editCategories} closeModal={()=>{
-    setEditCategories(false);
-  }}>
-    <EditCategories/>
-  </Modal>}
+        {editCategories && (
+          <Modal
+            title={"Edit Categories"}
+            isOpen={editCategories}
+            closeModal={() => {
+              setEditCategories(false);
+            }}
+          >
+            <EditCategories />
+          </Modal>
+        )}
       </div>
     </>
   );
