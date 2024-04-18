309e9ab3f73a747d471649d4aea7b1446e023810
ashhar <ashharulhaque@gmail.com>
2024-04-18 11:49:04 +0530

    tabs added

--- src/app/(routescomponent)/restaurants/restaurant/orders/page.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/orders/page.jsx
@@ -2,9 +2,10 @@
 
 function Orders() {
   return (
-    <div className="bg-green-400">
-      <ul className="bg-red-400 flex justify-center w-full min-h-[13dvh]">
-        <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">ALL Orders</li>
+    <div className="bg-green-400 flex flex-col ">
+     
+      <ul className="bg-red-400 flex justify-center  min-h-[13dvh] ">
+        <li className="bg-yellow-300 min-w-[15%] m-3 p-2  rounded-md flex justify-center items-center">ALL Orders</li>
         <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">Dine In Orders</li>
         <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">Take Away Orders</li>
         <li className="bg-yellow-300 min-w-[15%] m-3 p-2 rounded-md flex justify-center items-center">Self Delievery Orders</li>
