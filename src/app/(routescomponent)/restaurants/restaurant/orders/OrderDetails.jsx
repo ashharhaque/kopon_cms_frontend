0cfb3cb5b7b0ff4da3d13c50a4831af7d7593068
ashhar <ashharulhaque@gmail.com>
2024-04-18 22:28:35 +0530

    some changes

--- src/app/(routescomponent)/restaurants/restaurant/orders/OrderDetails.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/orders/OrderDetails.jsx
@@ -1,7 +1,17 @@
-function OrderDetails({className}) {
+function OrderDetails({ className }) {
   return (
-    <div className={`${className}`}>OrderDetails</div>
-  )
+    <div className={`${className}`}>
+      <ul className="flex justify-around divide-x-2 border-b-2">
+        <li className="divide-y p-2 m-2 "><p className="border-b-2">Order Id:</p><p className="text-center">111</p></li>
+        <li className="divide-y p-2 m-2"><p className="border-b-2">Ordered At:</p><p className="text-center">11:00 A.M</p></li>
+        <li className="divide-y p-2 m-2"><p className="border-b-2">Accepted At:</p><p className="text-center">11:10 A.M</p></li>
+        <li className="divide-y p-2 m-2"><p className="border-b-2">Order Type:</p><p className="text-center">Dine-In</p></li>
+        <li className="divide-y p-2 m-2"><p className="border-b-2">Order Status:</p><p className="text-center">In-Process</p></li>
+        <li className="divide-y p-2 m-2"><p className="border-b-2 flex justify-center">Total Duration:</p><p className="text-center">00 hrs:12 mins</p></li>
+
+      </ul>
+    </div>
+  );
 }
 
-export default OrderDetails
+export default OrderDetails;
