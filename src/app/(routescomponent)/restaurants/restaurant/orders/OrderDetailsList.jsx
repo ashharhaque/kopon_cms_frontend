97339d6ca324a0bd6440d69360de596bc00e0458
ashhar <ashharulhaque@gmail.com>
2024-04-20 11:34:29 +0530

    done

--- src/app/(routescomponent)/restaurants/restaurant/orders/OrderDetailsList.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/orders/OrderDetailsList.jsx
@@ -1,30 +1,34 @@
 function OrderDetailsList({ className }) {
   return (
     <div className={`${className}`}>
-      <table className="flex border-spacing-3 justify-around">
-        <thead>
-          <td>Product Name</td>
-          <td>Category</td>
-          <td>VEG/NON VEG</td>
-          <td>PRICE</td>
-          <td>QTY</td>
-          <td>Tax Percentage</td>
-          <td>Tax</td>
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 overflow-scroll">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 overflow-scroll">
+          <tr>
+            <th  scope="col" className="px-6 py-3">Product Name</th>
+            <th scope="col" className="px-6 py-3">Category</th>
+            <th scope="col" className="px-6 py-3">VEG/NON VEG</th>
+            <th scope="col" className="px-6 py-3">PRICE</th>
+            <th scope="col" className="px-6 py-3">QTY</th>
+            <th scope="col" className="px-6 py-3">Tax Percentage</th>
+            <th scope="col" className="px-6 py-3">Tax</th>
 
-          <td>CHARGES</td>
-          <td>Total Price</td>
+            <th scope="col" className="px-6 py-3">CHARGES</th>
+            <th scope="col" className="px-6 py-3">Total Price</th>
+          </tr>
         </thead>
 
         <tbody>
-          <tr>
-            <td>Margheretta Pizza</td>
-            <td>Pizza</td>
-            <td>Veg</td>
-            <td>Rs.123</td>
-            <td>3</td>
-            <td>30%</td>
-            <td>Rs.50</td>
-            <td>Rs.173</td>
+          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Margheretta Pizza</th>
+            <td className="px-6 py-4">Pizza</td>
+            <td className="px-6 py-4">Veg</td>
+            <td className="px-6 py-4">Rs.123</td>
+            <td className="px-6 py-4">3</td>
+            <td className="px-6 py-4">30%</td>
+            <td className="px-6 py-4">Rs.50</td>
+            <td className="px-6 py-4">Rs.173</td>
+            <td className="px-6 py-4">Rs.203</td>
+
           </tr>
         </tbody>
       </table>
