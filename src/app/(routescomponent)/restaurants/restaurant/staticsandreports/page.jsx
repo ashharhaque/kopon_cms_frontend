ea3a7720a2b4c6a5930f2f69384e682b9dec8d91
ashhar <ashharulhaque@gmail.com>
2024-04-22 09:24:10 +0530

    statics

--- src/app/(routescomponent)/restaurants/restaurant/staticsandreports/page.jsx
+++ src/app/(routescomponent)/restaurants/restaurant/staticsandreports/page.jsx
@@ -1,9 +1,10 @@
+import Link from "next/link"
 function Statics() {
   return (
     <div className="flex justify-center items-center bg-green-400 min-h-[55dvh]">
       <ul className="flex justify-center">
         <li className="bg-red-400 w-[40dvw] h-[20dvh] flex justify-center items-center rounded-full mx-4">REPORTS</li>
-        <li className="bg-yellow-300 w-[40dvw] h-[20dvh] flex justify-center items-center rounded-full">STATICS</li>
+        <Link href="/restaurants/restaurant/staticsandreports/statics"><li className="bg-yellow-300 w-[40dvw] h-[20dvh] flex justify-center items-center rounded-full">STATICS</li></Link>
       </ul>
     </div>
   )
---
+++ src/app/(routescomponent)/restaurants/restaurant/staticsandreports/statics/page.jsx
@@ -0,0 +1,9 @@
+
+
+function Statics() {
+  return (
+    <div>Statics</div>
+  )
+}
+
+export default Statics
